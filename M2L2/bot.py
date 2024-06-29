4098d56d57fecbc6fa3ddd00b362517a9197e85f
Артем <timoshenkaa@inbox.ru>
2024-06-29 17:18:05 +0300

    Add files via upload

---
+++ M2L2/bot.py
@@ -0,0 +1,41 @@
+# Бот, который считает, сколько разлагается вещь
+
+import discord
+from discord.ext import commands
+import random
+
+intents = discord.Intents.default()
+intents.message_content = True
+bot = commands.Bot(command_prefix='$', intents=intents)
+
+# Код
+items = {
+    'пластик': 450,
+    'стекло': 'Триллион миллиардов',
+    'полиэтилен': 20,
+    'алюминий': 318,
+    'подгузник': 500
+}
+
+@bot.command()
+async def howlong(ctx, item):
+    item = item.lower()
+    if item in items.keys():
+        await ctx.send(f'Вещь {item} разлагается {items[item]} лет')
+    else:
+        await ctx.send('Такой вещи нет в базе данных')
+
+
+# Бот, который дает советы по экологии
+@bot.command()
+async def eco(ctx):
+    facts = [
+        "Каждый год в океаны попадает около 8 миллионов тонн пластика.",
+        "Леса покрывают около 30% поверхности земли и являются домом для 80% наземных видов животных и растений.",
+        "Более миллиарда людей живут без доступа к чистой питьевой воде.",
+        "Один рециклированный стеклянный бутыль экономит достаточно энергии, чтобы запитать лампу на 4 часа.",
+        "Каждое сохраненное дерево может поглотить около 21 кг углекислого газа в год."
+    ]
+    await ctx.send(f'Рандомный факт: {random.choice(facts)}')
+
+bot.run('MTI0OTAwMDkzMTQ5NzA4NzAwNg.G6cg6-.L8RBzpw56Ab3s-dz1jKqMkvHzHNMCYlh18tLco')
